1812aa5ec86f44704c0ba249c67c8649ebf9e850
Antoniu Sergiu <antoniu.sergiu2002@gmail.com>
2026-09-09 21:14:55 +0300

    new player

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -2,25 +2,11 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [Header("Health")]
-    [SerializeField]private int currentHealth;
-    [SerializeField] private int maxHealth;
+    [Header("Player components")]
+    public PlayerInventory inventory;
+    public PlayerStats stats;
+    public PlayerVisuals visuals;
 
-    //Player UI
-    [Header("Components")]
-    //Get refrences from every script that the player uses
-    [SerializeField] private PlayerInventory inventory;
+    public GameInput gameInput;
 
-    public void RestoreHealth(int amount)
-    {
-        int adaosHealth= currentHealth += amount;
-        if (adaosHealth > maxHealth)
-        {
-            currentHealth = maxHealth;
-        }
-        else
-        {
-            currentHealth += amount;
-        }
-    }
 }
